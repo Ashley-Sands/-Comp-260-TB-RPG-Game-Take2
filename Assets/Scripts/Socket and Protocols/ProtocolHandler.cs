96a577e16bcdfc246c0de3049421230804134a68
Ashley Sands <ashsands1@amstrike.com>
2020-03-23 23:24:10 +0000

    added Protocol hander and base protocol

---
+++ Assets/Scripts/Socket and Protocols/ProtocolHandler.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Protocol
+{
+
+	public delegate void protocol_event ( BaseProtocol protocol );
+
+    /// <summary>
+    /// Sigleton class for handlering all protocols.
+    /// use Bind/Unbind to connect functions for each protocol
+    /// </summary>
+	public class ProtocolHandler
+	{
+        /// <summary>
+        /// Event system for each protocol
+        /// </summary>
+        private class ProtocolEvent
+        {
+            public event protocol_event callback;
+
+            public void Invoke ( BaseProtocol protocol )
+            {
+                callback?.Invoke( protocol );
+            }
+
+        }
+
+        Dictionary<char, ProtocolEvent> protocolEvents;
+
+        // singleton instance
+        private static ProtocolHandler inst;
+        public static ProtocolHandler Inst {
+            get {
+                if ( inst == null )
+                    inst = new ProtocolHandler();
+
+                return inst;
+            }
+
+        }
+
+        private ProtocolHandler ()
+        {
+            // see protocol list in server project for a full description of each protocol.
+            protocolEvents = new Dictionary<char, ProtocolEvent>
+            {
+                // Genral Commands
+                { ' ', new ProtocolEvent() }
+
+                // Dont forget to add it to Convert json as well :)
+            };
+
+        }
+
+        /// <summary>
+        /// Binds function to protocol callback
+        /// </summary>
+        /// <param name="idenity">the idenity to bind to</param>
+        /// <param name="protocolFunc">function to bind</param>
+        public void Bind ( char idenity, protocol_event protocolFunc )
+        {
+
+            if ( !protocolEvents.ContainsKey( idenity ) )
+            {
+                Debug.LogErrorFormat( "Unable to bind, Failed to identify protocol {0}", idenity );
+                return;
+            }
+
+            protocolEvents[ idenity ].callback += protocolFunc;
+
+        }
+
+        /// <summary>
+        /// Unbinds function to protocol callback
+        /// </summary>
+        /// <param name="idenity">the idenity to bind to</param>
+        /// <param name="protocolFunc">function to bind</param>
+        public void Unbind ( char idenity, protocol_event protocolFunc )
+        {
+
+            if ( !protocolEvents.ContainsKey( idenity ) )
+            {
+                Debug.LogErrorFormat( "Unable to unbind, Failed to identify protocol {0}", idenity );
+                return;
+            }
+
+            protocolEvents[ idenity ].callback -= protocolFunc;
+
+        }
+
+        public void InvokeProtocol ( BaseProtocol proto )
+        {
+            protocolEvents[ proto.Identity ].Invoke( proto );
+        }
+
+        /// <summary>
+        /// Handles json string as idenity.
+        /// </summary>
+        /// <param name="idenity">idenity of the json string</param>
+        /// <param name="json">json string of idenity</param>
+        /// <returns> protocol. null if protocol does not exist</returns>
+        public static BaseProtocol ConvertJson ( char idenity, string json )
+        {
+
+            BaseProtocol newProto;
+
+            switch ( idenity )
+            {
+                default:    // Not found
+                    Debug.LogErrorFormat( "Unable to handle json, Failed to identify protocol {0}", idenity );
+                    return null;
+            }
+
+            return newProto;
+
+        }
+
+    }
+}
---
+++ Assets/Scripts/Socket and Protocols/Protocols/BaseProtocol.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Protocol
+{
+    public abstract class BaseProtocol
+    {
+
+        public abstract char Identity { get; }
+
+        public string from_client_name = "";         // this is the display from name :)
+
+        private int chached_jsonLength = 0;
+
+        public T AsType<T>() where T : BaseProtocol
+        {
+            if ( this is T )
+            {
+                return this as T;
+            }
+            else
+            {
+                Debug.LogErrorFormat( "Unable to get {0}, as tpye T ({1})", GetType(), typeof( T ) );
+                return null;
+            }
+                        
+        }
+
+        /// <summary>
+        /// Gets this class in json format :)
+        /// </summary>
+        /// <param name="jsonLength">OUT the length of the message</param>
+        /// <returns>this class as json</returns>
+        public string GetJson ( out int jsonLength )
+        {
+            string jsonStr = JsonUtility.ToJson( this );
+            chached_jsonLength = jsonLength = jsonStr.Length;
+
+            return jsonStr;
+        }
+
+        /// <summary>
+        /// Gets the chached message length from the last time GetMessage was called
+        /// </summary>
+        /// <returns>chached message length</returns>
+        public int GetJsonLength ()
+        {
+            return chached_jsonLength;
+        }
+
+    }
+}
