26146179ee76598c6672354836753e7c56d485d6
Ashley Sands <ashsands1@amstrike.com>
2020-03-25 03:18:31 +0000

    Added lobbies scene and set nickname

---
+++ Assets/Scripts/Componets/UI Actions/UIAct_setNickname.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIAct_setNickname : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI text;
+
+    private void Start ()
+    {
+
+        text.SetText( GameCtrl.Inst.playerData.nickname );
+
+    }
+}
