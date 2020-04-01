9116e247594b79c0b3a59497e0c67d4a5a30eabc
Ashley Sands <ashsands1@amstrike.com>
2020-04-01 23:18:33 +0100

    added default scene :) we're gettig there

---
+++ Assets/Scripts/Componets/ClientAgent.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+[RequireComponent( typeof( NavMeshAgent ) )]
+public class ClientAgent : MonoBehaviour
+{
+
+    private NavMeshAgent agent;
+
+    [SerializeField] private float moveSpeed = 5;
+
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Start ()
+    {
+        
+    }
+
+    private void Update ()
+    {
+        
+    }
+
+    private void GetHitLocation()
+    {
+
+    }
+
+    private void MoveAgent( Protocol.BaseProtocol proto )
+    {
+
+    }
+
+    private void OnDestroy ()
+    {
+        
+    }
+
+}
