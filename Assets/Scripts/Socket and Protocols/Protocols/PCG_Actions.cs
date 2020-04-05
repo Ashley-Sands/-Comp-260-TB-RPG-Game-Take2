e045ca9ab7b849c7547fcd6bf06557f433fb596e
Ashley Sands <ashsands1@amstrike.com>
2020-04-05 22:50:42 +0100

    added move player protocol

---
+++ Assets/Scripts/Socket and Protocols/Protocols/PCG_Actions.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Protocol
+{
+    public abstract class BaseGameAction : BaseProtocol
+    {
+
+        public int player_id;
+
+    }
+
+    public class MovePlayer : BaseGameAction
+    {
+
+        public override char Identity => 'M';
+
+        public Vector3 Position
+        {
+            get => new Vector3( x, y, z );
+            set{
+                x = value.x;
+                y = value.y;
+                z = value.z;
+            }
+        }
+
+        public float x, y, z;
+
+    }
+}
