6b1e3dafe6f8d7576194f5b6188d448a5f9bfcaf
Ashley Sands <ashsands1@amstrike.com>
2020-04-06 20:37:25 +0100

    added send scene loaded on awake

---
+++ Assets/Scripts/Componets/SendSceneLoaded_OnAwake.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendSceneLoaded_OnAwake : MonoBehaviour
+{
+	private void Awake ()
+	{
+
+		Protocol.ClientStatus status = new Protocol.ClientStatus()
+		{
+			StatusType = Protocol.ClientStatusType.SceneLoaded,
+			ok = true,
+			message = gameObject.scene.name
+		};
+
+		status.Send();
+
+	}
+}
