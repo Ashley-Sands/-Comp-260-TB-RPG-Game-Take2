562c9805b2ac5cdd267ee0cec296c66a19970a25
Ashley Sands <ashsands1@amstrike.com>
2020-04-07 23:18:54 +0100

    added serverObject and protocol

--- Assets/Scripts/Socket and Protocols/Protocols/PCG_Actions.cs
+++ Assets/Scripts/Socket and Protocols/Protocols/PCG_Actions.cs
@@ -29,4 +29,40 @@ namespace Protocol
         public float x, y, z;
 
     }
+
+    // Altho this inherits from BaseProtocol its an action that can only happen in the game
+    public class ServerObject : BaseProtocol 
+    {
+        public enum ObjectType { Player = 0, Relic = 1 }
+        public override char Identity => '#';
+
+        public Vector3 Position {
+            get => new Vector3( x, y, z );
+            set {
+                x = value.x;
+                y = value.y;
+                z = value.z;
+            }
+        }
+
+        public ObjectType Type{
+            get{ return (ObjectType)type; }
+            set{ type = (int)value; }
+        }
+
+        public float x, y, z;
+
+        public int type;
+        public int object_id = -1;
+
+        public ServerObject( Vector3 _position, ObjectType _type, int _objId )
+        {
+            Position = _position;
+            Type = _type;
+            object_id = _objId;
+        }
+
+
+
+    }
 }
---
+++ Assets/Scripts/Socket and Protocols/ServerObject.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ServerObject : MonoBehaviour
+{
+
+    [SerializeField] private Protocol.ServerObject.ObjectType serverObjectType;
+    private int serverObjectId = -1;
+    private Vector3 lastPosition;
+
+    private void Start ()
+    {
+        Protocol.ProtocolHandler.Inst.Bind('#', UpdateServerObject);
+    }
+
+    /// <summary>
+    /// Updates the objects position when the server says so. :D
+    /// </summary>
+    /// <param name="proto"></param>
+    public void UpdateServerObject( Protocol.BaseProtocol proto )
+    {
+
+        Protocol.ServerObject obj = proto.AsType<Protocol.ServerObject>();
+
+        if ( obj.Type != serverObjectType || obj.object_id != serverObjectId )
+            return;
+
+        // update the position of the object.
+        lastPosition = transform.position = obj.Position;
+
+    }
+
+    /// <summary>
+    /// Sends the ServerObject Protocol for this object to the server :)
+    /// </summary>
+    public virtual void Send()
+    {
+        if ( transform.position == lastPosition ) return;
+
+        Protocol.ServerObject obj = new Protocol.ServerObject( transform.position, serverObjectType, serverObjectId );
+        obj.Send();
+
+        lastPosition = transform.position;
+
+    }
+
+    private void OnDestroy ()
+    {
+        Protocol.ProtocolHandler.Inst.Unbind('#', UpdateServerObject);
+    }
+}
