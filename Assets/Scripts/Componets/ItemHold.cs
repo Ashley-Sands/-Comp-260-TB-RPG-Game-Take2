e612fe04e4aadc7fbd3d1c27f0027482469d3c39
Ashley Sands <ashsands1@amstrike.com>
2020-04-07 21:53:09 +0100

    added carry item

--- Assets/Scripts/Componets/ItemHold.cs
+++ Assets/Scripts/Componets/ItemHold.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class ItemHold : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private GameObject currentCarryItem; 
 }
