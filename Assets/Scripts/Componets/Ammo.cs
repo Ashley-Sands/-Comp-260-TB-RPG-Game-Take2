3e1159d5f9076e62c25c93e0e06ff49e163370c2
Ashley Sands <ashsands1@amstrike.com>
2020-04-07 21:49:32 +0100

    added basic outline for ammo / weapon

---
+++ Assets/Scripts/Componets/Ammo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Ammo : MonoBehaviour
+{
+
+    [SerializeField] private GameObject projectilePrefab;
+    [SerializeField] private int currentAmmo = 15;
+    [SerializeField] private TextMeshProUGUI ammoText;
+
+    private void UpdateUi()
+    {
+        if ( ammoText != null )
+            ammoText.text = currentAmmo.ToString();
+    }
+    
+}
