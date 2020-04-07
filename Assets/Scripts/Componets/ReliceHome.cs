4c0f57ff95c39bea81de78abb6ef42f6c319688b
Ashley Sands <ashsands1@amstrike.com>
2020-04-07 22:04:50 +0100

    added update ui to relics

--- Assets/Scripts/Componets/ReliceHome.cs
+++ Assets/Scripts/Componets/ReliceHome.cs
@@ -1,18 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ReliceHome : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    [SerializeField] private int compleatRelices = 4;
+    private int currentRelices = 0;
+
+    [SerializeField] private TextMeshProUGUI relicText;
+
+    private void OnTriggerEnter ( Collider other )
     {
-        
+        if ( other.CompareTag( "Relic" ) )
+        {
+            ++currentRelices;
+
+            if ( currentRelices == compleatRelices )
+            {
+                // compleat game.
+            }
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit ( Collider other )
     {
-        
+        if ( other.CompareTag( "Relic" ) )
+        {
+            --currentRelices;
+        }
+    }
+
+    private void UpdateUi()
+    {
+        if ( relicText != null )
+            relicText.text = currentRelices.ToString();
     }
 }
