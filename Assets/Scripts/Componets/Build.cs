74820a3eb4c4b84e6ce74e236d2a64c79403604e
Ashley Sands <ashsands1@amstrike.com>
2020-04-07 21:51:31 +0100

    added basic outline for build items

---
+++ Assets/Scripts/Componets/Build.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Build : MonoBehaviour
+{
+    [SerializeField] private GameObject wallPrefab;
+    [SerializeField] private int buildItems = 3;
+    [SerializeField] private TextMeshProUGUI buildText;
+
+    private void UpdateUi()
+    {
+        if ( buildText != null )
+            buildText.text = buildItems.ToString();
+    }
+}
