c8aa5637bbf1151e4e4b23d834ab25a1606d8382
Ashley Sands <ashsands1@amstrike.com>
2020-04-11 04:07:37 +0100

    added follow object

---
+++ Assets/Scripts/Componets/FollowObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowObject : MonoBehaviour
+{
+    [SerializeField] private Transform objectToFollow;
+
+    void Update()
+    {
+        if ( objectToFollow != null )
+            transform.position = objectToFollow.position;    
+    }
+}
