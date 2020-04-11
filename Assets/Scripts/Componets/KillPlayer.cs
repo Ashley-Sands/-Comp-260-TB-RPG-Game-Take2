214d7b080c52b87572ee65f4bbd15f74cdd6d044
Ashley Sands <ashsands1@amstrike.com>
2020-04-11 03:46:57 +0100

    added kil player

---
+++ Assets/Scripts/Componets/KillPlayer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Health) )]
+public class KillPlayer : MonoBehaviour
+{
+
+    [SerializeField] private Transform playerCamHold;
+    [SerializeField] private GameObject deadedObject;
+
+    private void Start ()
+    {
+        GetComponent<Health>().HealthDepleated += Kill;
+    }
+
+    private void Kill()
+    {
+
+        if (playerCamHold)
+            playerCamHold.parent = null;
+
+        Vector3 position = transform.position;
+        position.y = 1f;
+
+        Instantiate( deadedObject, position, transform.rotation );
+
+        Destroy( gameObject );
+
+    }
+
+    private void OnDestroy ()
+    {
+        GetComponent<Health>().HealthDepleated -= Kill;
+    }
+
+}
