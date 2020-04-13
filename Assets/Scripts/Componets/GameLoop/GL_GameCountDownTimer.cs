7f259040e7db46fd442f545e58eef5b6dd9c6b3a
Ashley Sands <ashsands1@amstrike.com>
2020-04-13 17:24:25 +0100

    added lerp Time pannel

--- Assets/Scripts/Componets/GameLoop/GL_GameCountDownTimer.cs
+++ Assets/Scripts/Componets/GameLoop/GL_GameCountDownTimer.cs
@@ -8,41 +8,92 @@ public class GL_GameCountDownTimer : MonoBehaviour
 
     [SerializeField] private TextMeshProUGUI timeText;
 
-    private Coroutine court;
+    [SerializeField] private Transform timerHold;
+    private Vector2 timerHoldStartPosition;
+    [SerializeField] private float timerHoldYLerpAmount = 50;
+    [SerializeField] private float timerHoldLerpTime = 0.5f;
+
+    [SerializeField] private GameObject upNextTextHold;
+    [SerializeField] private GameObject timesUpTextHold;
+
+    private Coroutine timerCourt;
 
     void Start()
     {
         GameCtrl.Inst.gameLoopEvent += UpdateGameInfo;
+        timerHoldStartPosition = timerHold.position;
     }
 
     private void UpdateGameInfo( Protocol.GameLoop.Actions gameAction, int time )
     {
-        if ( court == null )
+        // update the timer
+        if ( timerCourt == null )
         {
-            court = StartCoroutine( Timer( gameAction.ToString(), time ) );
+            timerCourt = StartCoroutine( Timer( time ) );
         }
         else
         {
-            StopCoroutine( court );
-            court = StartCoroutine( Timer( gameAction.ToString(), time ) );
+            StopCoroutine( timerCourt );
+            timerCourt = StartCoroutine( Timer( time ) );
         }
+
+        // Lerp in the change and end turn pannel.
+        switch ( gameAction )
+        {
+            case Protocol.GameLoop.Actions.End:     // lerp in on end
+                StartCoroutine( LerpTimerHold( true ) );
+                // togle the correct text
+                timesUpTextHold.SetActive( true );
+                upNextTextHold.SetActive( false );
+                break;
+            case Protocol.GameLoop.Actions.Start:   // lerp out on start
+                StartCoroutine( LerpTimerHold( false ) );
+                // togle the correct text
+                timesUpTextHold.SetActive( false );
+                upNextTextHold.SetActive( true );
+                break;
+        }
+
     }
 
-    private IEnumerator Timer( string lable, float ttl )
+    private IEnumerator Timer( float ttl )
     {
-        lable = lable + " - ({0}) ";
 
         while ( ttl > -1 )
         {
 
-            timeText.text = string.Format(lable, ttl);
+            timeText.text = string.Format( "{0}", ttl);
 
             yield return new WaitForSeconds( 1f );
             ttl -= 1f;
 
         }
 
-        timeText.text = timeText.text = string.Format( lable, 0 ); ;
+        timeText.text = string.Format( "{0}", 0 ); ;
+
+    }
+
+    private IEnumerator LerpTimerHold (bool scrollIn)
+    {
+        float offset = 0;
+        float timeElapsed = 0;
+        float updateRate = 1f / 60f;
+        YieldInstruction wait = new WaitForSeconds( updateRate );
+
+        while ( timeElapsed > 0f)
+        {
+
+            yield return wait;
+            timeElapsed += updateRate;
+
+            if ( scrollIn )
+                offset = Mathf.Lerp( 0, timerHoldYLerpAmount, timeElapsed / timerHoldLerpTime );
+            else
+                offset = Mathf.Lerp( timerHoldYLerpAmount, 0, timeElapsed / timerHoldLerpTime );
+
+            timerHold.position = timerHoldStartPosition + new Vector2( 0, offset );
+
+        }
 
     }
 
